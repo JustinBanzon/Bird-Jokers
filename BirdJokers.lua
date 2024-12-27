847565789da413380627cdf16e13832b6e5e323e
JustinBanzon <banzoj@rpi.edu>
2024-12-26 22:46:20 -0500

    House Sparrow update, runtime optimization

--- BirdJokers.lua
+++ BirdJokers.lua
@@ -479,7 +479,7 @@ local house_sparrow = SMODS.Joker{key="house_sparrow",
             'all {C:attention}Showdown blinds'
         }},
     loc_vars = function(self, info_queue, card)
-        local showdown = (G.GAME.round_resets.ante and (G.GAME.win_ante + math.max(0, math.floor(G.GAME.round_resets.ante / G.GAME.win_ante) * G.GAME.win_ante))) or 8 
+        local showdown = (G.GAME.round_resets.ante and (G.GAME.win_ante + math.max(0, math.floor((G.GAME.round_resets.ante-1) / G.GAME.win_ante) * G.GAME.win_ante))) or 8 
         info_queue[#info_queue+1] = {set = 'Other', key = 'showdowns', vars = {showdown}}
         return {vars = {card.ability.mult}}
     end,
@@ -1161,82 +1161,84 @@ local canary = SMODS.Joker{
 --- create_card_alt (used by some jokers) ---
 -- Credit to the creators of Joker Evolution for the code
 
-function create_card_alt(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append, edition_append, forced_edition)
-    local area = area or G.jokers
-    local center = G.P_CENTERS.b_red
-        
+if not create_card_alt then 
+    function create_card_alt(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append, edition_append, forced_edition)
+        local area = area or G.jokers
+        local center = G.P_CENTERS.b_red
+            
 
-    --should pool be skipped with a forced key
-    if not forced_key and soulable and (not G.GAME.banned_keys['c_soul']) then
-        if (_type == 'Tarot' or _type == 'Spectral' or _type == 'Tarot_Planet') and
-        not (G.GAME.used_jokers['c_soul'] and not next(find_joker("Showman")))  then
-            if pseudorandom('soul_'.._type..G.GAME.round_resets.ante) > 0.997 then
-                forced_key = 'c_soul'
+        --should pool be skipped with a forced key
+        if not forced_key and soulable and (not G.GAME.banned_keys['c_soul']) then
+            if (_type == 'Tarot' or _type == 'Spectral' or _type == 'Tarot_Planet') and
+            not (G.GAME.used_jokers['c_soul'] and not next(find_joker("Showman")))  then
+                if pseudorandom('soul_'.._type..G.GAME.round_resets.ante) > 0.997 then
+                    forced_key = 'c_soul'
+                end
+            end
+            if (_type == 'Planet' or _type == 'Spectral') and
+            not (G.GAME.used_jokers['c_black_hole'] and not next(find_joker("Showman")))  then 
+                if pseudorandom('soul_'.._type..G.GAME.round_resets.ante) > 0.997 then
+                    forced_key = 'c_black_hole'
+                end
             end
         end
-        if (_type == 'Planet' or _type == 'Spectral') and
-        not (G.GAME.used_jokers['c_black_hole'] and not next(find_joker("Showman")))  then 
-            if pseudorandom('soul_'.._type..G.GAME.round_resets.ante) > 0.997 then
-                forced_key = 'c_black_hole'
+
+        if _type == 'Base' then 
+            forced_key = 'c_base'
+        end
+
+        if forced_key and not G.GAME.banned_keys[forced_key] then 
+            center = G.P_CENTERS[forced_key]
+            _type = (center.set ~= 'Default' and center.set or _type)
+        else
+            local _pool, _pool_key = get_current_pool(_type, _rarity, legendary, key_append)
+            center = pseudorandom_element(_pool, pseudoseed(_pool_key))
+            local it = 1
+            while center == 'UNAVAILABLE' do
+                it = it + 1
+                center = pseudorandom_element(_pool, pseudoseed(_pool_key..'_resample'..it))
             end
-        end
-    end
 
-    if _type == 'Base' then 
-        forced_key = 'c_base'
-    end
-
-    if forced_key and not G.GAME.banned_keys[forced_key] then 
-        center = G.P_CENTERS[forced_key]
-        _type = (center.set ~= 'Default' and center.set or _type)
-    else
-        local _pool, _pool_key = get_current_pool(_type, _rarity, legendary, key_append)
-        center = pseudorandom_element(_pool, pseudoseed(_pool_key))
-        local it = 1
-        while center == 'UNAVAILABLE' do
-            it = it + 1
-            center = pseudorandom_element(_pool, pseudoseed(_pool_key..'_resample'..it))
+            center = G.P_CENTERS[center]
         end
 
-        center = G.P_CENTERS[center]
-    end
+        local front = ((_type=='Base' or _type == 'Enhanced') and pseudorandom_element(G.P_CARDS, pseudoseed('front'..(key_append or '')..G.GAME.round_resets.ante))) or nil
 
-    local front = ((_type=='Base' or _type == 'Enhanced') and pseudorandom_element(G.P_CARDS, pseudoseed('front'..(key_append or '')..G.GAME.round_resets.ante))) or nil
+        local card = Card(area.T.x + area.T.w/2, area.T.y, G.CARD_W, G.CARD_H, front, center,
+        {bypass_discovery_center = area==G.shop_jokers or area == G.pack_cards or area == G.shop_vouchers or (G.shop_demo and area==G.shop_demo) or area==G.jokers or area==G.consumeables,
+         bypass_discovery_ui = area==G.shop_jokers or area == G.pack_cards or area==G.shop_vouchers or (G.shop_demo and area==G.shop_demo),
+         discover = area==G.jokers or area==G.consumeables, 
+         bypass_back = G.GAME.selected_back.pos})
+        if card.ability.consumeable and not skip_materialize then card:start_materialize() end
 
-    local card = Card(area.T.x + area.T.w/2, area.T.y, G.CARD_W, G.CARD_H, front, center,
-    {bypass_discovery_center = area==G.shop_jokers or area == G.pack_cards or area == G.shop_vouchers or (G.shop_demo and area==G.shop_demo) or area==G.jokers or area==G.consumeables,
-     bypass_discovery_ui = area==G.shop_jokers or area == G.pack_cards or area==G.shop_vouchers or (G.shop_demo and area==G.shop_demo),
-     discover = area==G.jokers or area==G.consumeables, 
-     bypass_back = G.GAME.selected_back.pos})
-    if card.ability.consumeable and not skip_materialize then card:start_materialize() end
-
-    if _type == 'Joker' then
-        if G.GAME.modifiers.all_eternal then
-            card:set_eternal(true)
-        end
-        if (area == G.shop_jokers) or (area == G.pack_cards) then 
-            local eternal_perishable_poll = pseudorandom((area == G.pack_cards and 'packetper' or 'etperpoll')..G.GAME.round_resets.ante)
-            if G.GAME.modifiers.enable_eternals_in_shop and eternal_perishable_poll > 0.7 then
+        if _type == 'Joker' then
+            if G.GAME.modifiers.all_eternal then
                 card:set_eternal(true)
-            elseif G.GAME.modifiers.enable_perishables_in_shop and ((eternal_perishable_poll > 0.4) and (eternal_perishable_poll <= 0.7)) then
-                card:set_perishable(true)
             end
-            if G.GAME.modifiers.enable_rentals_in_shop and pseudorandom((area == G.pack_cards and 'packssjr' or 'ssjr')..G.GAME.round_resets.ante) > 0.7 then
-                card:set_rental(true)
+            if (area == G.shop_jokers) or (area == G.pack_cards) then 
+                local eternal_perishable_poll = pseudorandom((area == G.pack_cards and 'packetper' or 'etperpoll')..G.GAME.round_resets.ante)
+                if G.GAME.modifiers.enable_eternals_in_shop and eternal_perishable_poll > 0.7 then
+                    card:set_eternal(true)
+                elseif G.GAME.modifiers.enable_perishables_in_shop and ((eternal_perishable_poll > 0.4) and (eternal_perishable_poll <= 0.7)) then
+                    card:set_perishable(true)
+                end
+                if G.GAME.modifiers.enable_rentals_in_shop and pseudorandom((area == G.pack_cards and 'packssjr' or 'ssjr')..G.GAME.round_resets.ante) > 0.7 then
+                    card:set_rental(true)
+                end
             end
-        end
 
-        if edition_append then
-            if forced_edition == nil then
-                local edition = poll_edition('edi'..(key_append or '')..G.GAME.round_resets.ante)
-                card:set_edition(edition)
-            else
-                card:set_edition(forced_edition)
+            if edition_append then
+                if forced_edition == nil then
+                    local edition = poll_edition('edi'..(key_append or '')..G.GAME.round_resets.ante)
+                    card:set_edition(edition)
+                else
+                    card:set_edition(forced_edition)
+                end
+                check_for_unlock({type = 'have_edition'})
             end
-            check_for_unlock({type = 'have_edition'})
         end
+        return card
     end
-    return card
 end
 ----------------------------------------------
 ------------MOD CODE END----------------------
